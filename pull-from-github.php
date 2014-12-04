3b99837400754c32c484ea1848fae98f4568bc8d
Niru Maheswaranathan <nirum@stanford.edu>
2014-12-03 22:35:48 -0800

    removing old php files

--- pull-from-github.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
-}
-
-?>hi 
